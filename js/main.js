a289a89f52d66b59c72835f0237697067138b575
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-11-02 15:11:23 +0100

    remove comment

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 
 for (let i = 1; i <= 100; i++) {
 
-    // STAMPA NUMERI DA 0 A 100 CONTROLLATO
-
     if ((i % 3 === 0) && (i % 5 === 0)) {
 
         console.log("FizzBuzz")
@@ -13,10 +11,13 @@ for (let i = 1; i <= 100; i++) {
         console.log("Buzz")
 
     } else if (i % 3 === 0) {
+
         console.log("Fizz")
 
     } else {
+
         console.log(i)
+        
     }
 
 }
