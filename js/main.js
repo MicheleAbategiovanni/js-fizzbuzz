abf6595906df9c1c5cb267d88b2c173e7a6018d1
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-11-02 19:39:11 +0100

    added bonus

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
-
+const containerEl = document.querySelector( ".container-fizz" );
 
 for (let i = 1; i <= 100; i++) {
 
     if ((i % 3 === 0) && (i % 5 === 0)) {
 
-        console.log("FizzBuzz")
+        containerEl.innerHTML += `<div class="card-fizz bg-danger">FIZZBUZZ</div>`;
+
+        console.log("FizzBuzz");
 
     } else if (i % 5 === 0) {
+
+        containerEl.innerHTML += `<div class="card-fizz bg-fz-buzz">BUZZ</div>`;
 
         console.log("Buzz")
 
     } else if (i % 3 === 0) {
+        containerEl.innerHTML += `<div class="card-fizz bg-fz-secondary">FIZZ</div>`;
 
         console.log("Fizz")
 
     } else {
+        containerEl.innerHTML += `<div class="card-fizz bg-fz-primary">${ i }</div>`;
 
         console.log(i)
-        
+
     }
 
 }
